f6e0d785080319aa5fc47986f23c62ce7a226288
Median72 <medvivan11@gmail.com>
2025-04-05 12:25:42 +0700

    Update ad.py

--- ad.py
+++ ad.py
@@ -1,19 +1,21 @@
 import re
 
-data = input()
-if re.match(r'^\d{2}\.\d{2}\.\d{4}$', data):
-    if re.match(r'\d{2}\.[0][1-9]\.\d{4}|\d{2}\.[1][0-2]\.\d{4}', data):
-        if re.match(r'[0][1-9]\.\d{2}\.\d{4}|[1][0-9]\.\d{2}\.\d{4}|[2][0-9]\.\d{2}\.\d{4}|[3][0]\.04\.\d{4}|[3][0]\.06\.\d{4}|[3][0]\.09\.\d{4}|[3][0]\.11\.\d{4}|[3][0-1]\.01\.\d{4}|[3][0-1]\.03\.\d{4}|[3][0-1]\.05\.\d{4}|[3][0-1]\.07\.\d{4}|[3][0-1]\.08\.\d{4}|[3][0-1]\.10\.\d{4}|[3][0-1]\.12\.\d{4}', data):
-            if re.match(r'\d{2}\.02\.d{4}', data):
-                if int(data[0:2]) <= 28 and (int(data[-4:]) % 4 != 0 or int(data[-4:0]) % 100 == 0 and int(data[-4:0]) % 400 != 0):
-                    print("verno")
-                elif int(data[0:2]) <= 29 and (int(data[-4:]) % 4 == 0 and int(data[-4:0]) % 100 != 0 or int(data[-4:0]) % 400 == 0):
-                    print("verno")
-                else:
-                    print("neverno")
-            else:
-                print("verno")
+def is_valid_date(date_str):
+    # Проверяем формат с помощью регулярного выражения
+    if not re.fullmatch(r'^(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[0-2])\.(\d{4})$', date_str):
+        return False
+
+    day, month, year = map(int, date_str.split('.'))
+    
+    # Проверка количества дней в месяце
+    if month == 2:  # Февраль
+        if (year % 400 == 0) or (year % 100 != 0 and year % 4 == 0):
+            max_days = 29  # Високосный год
         else:
-            print("neverno")
+            max_days = 28  # Не високосный
+    elif month in [4, 6, 9, 11]:
+        max_days = 30  # Апрель, июнь, сентябрь, ноябрь
     else:
-        print("neverno")
+        max_days = 31  # Остальные месяцы
+    
+    return 1 <= day <= max_days
